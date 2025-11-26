3d6edff3e6dc7b9e83fc94cd22ad62e32e81e0c6
Mikalai Kazlou <mikalai_kazlou@epam.com>
2025-11-26 21:18:06 +0300

    feat: Provide view synchronisation for the List-Detail application

--- webapp/Component.js
+++ webapp/Component.js
@@ -2,34 +2,42 @@
  * eslint-disable @sap/ui5-jsdocs/no-jsdoc
  */
 
-sap.ui.define([
-        "sap/ui/core/UIComponent",
-        "sap/ui/Device",
-        "student/com/sap/training/advancedsapui5/listdetail/model/models"
-    ],
-    function (UIComponent, Device, models) {
-        "use strict";
+sap.ui.define(
+  [
+    "sap/ui/core/UIComponent",
+    "sap/ui/Device",
+    "student/com/sap/training/advancedsapui5/listdetail/model/models",
+    "student/com/sap/training/advancedsapui5/listdetail/controller/ListSelector",
+  ],
+  function (UIComponent, Device, models, ListSelector) {
+    "use strict";
 
-        return UIComponent.extend("student.com.sap.training.advancedsapui5.listdetail.Component", {
-            metadata: {
-                manifest: "json"
-            },
+    return UIComponent.extend(
+      "student.com.sap.training.advancedsapui5.listdetail.Component",
+      {
+        metadata: {
+          manifest: "json",
+        },
 
-            /**
-             * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
-             * @public
-             * @override
-             */
-            init: function () {
-                // call the base component's init function
-                UIComponent.prototype.init.apply(this, arguments);
+        /**
+         * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+         * @public
+         * @override
+         */
+        init: function () {
+          // call the base component's init function
+          UIComponent.prototype.init.apply(this, arguments);
 
-                // enable routing
-                this.getRouter().initialize();
+          // instantiation of the listselector
+          this.oListSelector = new ListSelector();
 
-                // set the device model
-                this.setModel(models.createDeviceModel(), "device");
-            }
-        });
-    }
+          // enable routing
+          this.getRouter().initialize();
+
+          // set the device model
+          this.setModel(models.createDeviceModel(), "device");
+        },
+      }
+    );
+  }
 );
