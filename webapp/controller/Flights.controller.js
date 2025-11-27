58f5455c8168fe281efb783d2d32bfa590a4279d
Mikalai Kazlou <mikalai_kazlou@epam.com>
2025-11-27 19:48:04 +0300

    feat: Extend an Existing Control

--- webapp/controller/Flights.controller.js
+++ webapp/controller/Flights.controller.js
@@ -1,65 +1,75 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, MessageToast) {
+    "use strict";
 
-        return Controller.extend("student00.com.sap.training.ux402.fullscreen.ux402fullscreen.controller.Flights", {
-            onInit: function () {
-                var oRouter = this.getRouter();
-                oRouter.getRoute("flights").attachMatched(this._onObjectMatched, this);
+    return Controller.extend(
+      "student00.com.sap.training.ux402.fullscreen.ux402fullscreen.controller.Flights",
+      {
+        onInit: function () {
+          var oRouter = this.getRouter();
+          oRouter
+            .getRoute("flights")
+            .attachMatched(this._onObjectMatched, this);
+        },
 
+        getRouter: function () {
+          return sap.ui.core.UIComponent.getRouterFor(this);
+        },
+
+        _onObjectMatched: function (oEvent) {
+          var oArgs = oEvent.getParameter("arguments");
+          this._sCarrierId = oArgs.carrid;
+          var oView = this.getView();
+
+          oView.bindElement({
+            path: "/UX_C_Carrier_TP('" + this._sCarrierId + "')",
+            events: {
+              change: this._onBindingChange.bind(this),
+              dataRequested: function () {
+                oView.setBusy(true);
+              },
+              dataReceived: function () {
+                oView.setBusy(false);
+              },
             },
+          });
+        },
 
-            getRouter: function () {
-                return sap.ui.core.UIComponent.getRouterFor(this);
-            },
+        _onBindingChange: function () {
+          const oElementBinding = this.getView().getElementBinding();
 
-            _onObjectMatched: function (oEvent) {
-                var oArgs = oEvent.getParameter("arguments");
-                this._sCarrierId = oArgs.carrid;
-                var oView = this.getView();
+          // No data for the binding
+          if (oElementBinding && !oElementBinding.getBoundContext()) {
+            this.getRouter().getTargets().display("notFound");
+          }
+        },
 
-                oView.bindElement({
-                    path: "/UX_C_Carrier_TP('" + this._sCarrierId + "')",
-                    events: {
-                        change: this._onBindingChange.bind(this),
-                        dataRequested: function () {
-                            oView.setBusy(true);
-                        },
-                        dataReceived: function () {
-                            oView.setBusy(false);
-                        }
-                    }
-                });
-            },
+        onNavBack: function () {
+          var oHistory, sPreviousHash;
 
-            _onBindingChange: function () {
-                const oElementBinding = this.getView().getElementBinding();
+          oHistory = sap.ui.core.routing.History.getInstance();
+          sPreviousHash = oHistory.getPreviousHash();
 
-                // No data for the binding 
-                if (oElementBinding && !oElementBinding.getBoundContext()) {
-                    this.getRouter().getTargets().display("notFound");
-                }
-            },
+          if (sPreviousHash !== undefined) {
+            window.history.go(-1);
+          } else {
+            this.getRouter().navTo("overview", true /*no history*/);
+          }
+        },
 
-            onNavBack: function () {
-                var oHistory, sPreviousHash;
-
-                oHistory = sap.ui.core.routing.History.getInstance();
-                sPreviousHash = oHistory.getPreviousHash();
-
-                if (sPreviousHash !== undefined) {
-                    window.history.go(-1);
-                } else {
-                    this.getRouter().navTo("overview", true /*no history*/);
-                }
-
-            }
-
-
-        });
-    });
+        onHover: function (evt) {
+          var sText = this.getOwnerComponent()
+            .getModel("i18n")
+            .getProperty("msgSeatsAv");
+          MessageToast.show(evt.getSource().getHoverText() + " " + sText, {
+            duration: 1000,
+          });
+        },
+      }
+    );
+  }
+);
