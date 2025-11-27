9f1e3b859e2ff8d9fa4a4a327acba36d10eb68cb
Mikalai Kazlou <mikalai_kazlou@epam.com>
2025-11-27 07:57:03 +0300

    feat: Add mobile behavior to the List-Detail application

--- webapp/controller/Flights.controller.js
+++ webapp/controller/Flights.controller.js
@@ -1,57 +1,52 @@
-sap.ui.define(
-  [
-    "student/com/sap/training/advancedsapui5/listdetail/controller/BaseController",
-  ],
-  function (Controller) {
-    "use strict";
+sap.ui.define(["student/com/sap/training/advancedsapui5/listdetail/controller/BaseController"], function (Controller) {
+  "use strict";
 
-    return Controller.extend(
-      "student.com.sap.training.advancedsapui5.listdetail.controller.Flights",
-      {
-        onInit: function () {
-          var oRouter = this.getRouter();
-          oRouter
-            .getRoute("flights")
-            .attachMatched(this._onObjectMatched, this);
+  return Controller.extend("student.com.sap.training.advancedsapui5.listdetail.controller.Flights", {
+    onInit: function () {
+      var oRouter = this.getRouter();
+      oRouter.getRoute("flights").attachMatched(this._onObjectMatched, this);
 
-          var oModel = this.getOwnerComponent().getModel();
-          oModel.attachRequestSent(function (oEvent) {
-            this.getView().setBusy(true);
-          }, this);
-          oModel.attachRequestCompleted(function (oEvent) {
-            this.getView().setBusy(false);
-          }, this);
+      var oModel = this.getOwnerComponent().getModel();
+      oModel.attachRequestSent(function (oEvent) {
+        this.getView().setBusy(true);
+      }, this);
+      oModel.attachRequestCompleted(function (oEvent) {
+        this.getView().setBusy(false);
+      }, this);
+    },
+
+    handleClose: function () {
+      var oView = this.getView();
+      oView.getModel("mainView").setProperty("/layout", "OneColumn");
+      this.getRouter().navTo("Overview", {}, true);
+    },
+
+    _onObjectMatched: function (oEvent) {
+      var oArgs = oEvent.getParameter("arguments");
+      this._sCarrierId = oArgs.carrid;
+      var oView = this.getView();
+
+      oView.getModel("mainView").setProperty("/layout", "TwoColumnsMidExpanded");
+
+      oView.bindElement({
+        path: "/UX_C_Carrier_TP('" + this._sCarrierId + "')",
+        events: {
+          change: this._onBindingChange.bind(this),
         },
+      });
+    },
 
-        _onObjectMatched: function (oEvent) {
-          var oArgs = oEvent.getParameter("arguments");
-          this._sCarrierId = oArgs.carrid;
-          var oView = this.getView();
+    _onBindingChange: function () {
+      var oView = this.getView();
+      var oElementBinding = oView.getElementBinding();
 
-          oView
-            .getModel("mainView")
-            .setProperty("/layout", "TwoColumnsMidExpanded");
-
-          oView.bindElement({
-            path: "/UX_C_Carrier_TP('" + this._sCarrierId + "')",
-            events: {
-              change: this._onBindingChange.bind(this),
-            },
-          });
-        },
-
-        _onBindingChange: function () {
-          var oView = this.getView();
-          var oElementBinding = oView.getElementBinding();
-          if (!oElementBinding.getBoundContext()) {
-            this.getRouter().getTargets().display("notFound");
-            this.getOwnerComponent().oListSelector.clearMasterListSelection();
-            return;
-          }
-          var sPath = oElementBinding.getPath();
-          this.getOwnerComponent().oListSelector.selectAListItem(sPath);
-        },
+      if (!oElementBinding.getBoundContext()) {
+        this.getRouter().getTargets().display("notFound");
+        this.getOwnerComponent().oListSelector.clearMasterListSelection();
+        return;
       }
-    );
-  }
-);
+      var sPath = oElementBinding.getPath();
+      this.getOwnerComponent().oListSelector.selectAListItem(sPath);
+    },
+  });
+});
